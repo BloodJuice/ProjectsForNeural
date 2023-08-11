90a1b0ddf96d1880d1e65f396e0231232986efb6
BloodJuice <falcon.with.hat@gmail.com>
2023-08-11 09:59:16 +0300

    It's done

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os, shutil, re
 from operator import itemgetter
 
-def search():
-    for adress, dirs, files in os.walk("E:\Нейронки\\r4"):
+def search(nameWay):
+    for adress, dirs, files in os.walk(nameWay):
         for file in files:
             if file.endswith('.txt'):
                 yield os.path.join(adress, file)
@@ -12,26 +12,29 @@ def read_from_pathtxt(path):
         for line in r:
             return line
 
+def readFileForAnalysys(path):
+    cloud = []
+    with open(path, 'r') as r:
+        for line in r:
+            cloud.append(line)
+    return cloud
+
 # Сортировка файлов, в которых задаваемые параметры числовые. Параметры типо True, False пропускаем мимо.
 def transformTextForNumberInCountValue(number1, number2, hub):
     for i in range(len(hub)):
         hub[i] = re.findall(r'\w+|[-+]?(?:\d+(?:\.\d*)?|\.\d+)', hub[i])
     for i in range(len(hub)):
-        # print("TRANSFORM:\t", hub[i])
         hub[i][number2] = float(hub[i][number2])
         if hub[i][number1] != 'True' and hub[i][number1] != 'False':
             if hub[i][number1] != 'caption_base_best' and hub[i][number1] != 'caption_huge_best':
                 hub[i][number1] = float(hub[i][number1])
     return hub
 
-def parserFirst(nameFile, group, folder):
+def parserFirst(nameWay, nameFile, group, folder):
     hubSlave, hubMain = [], []
     counter = 1
-    for i in search():
-        # print(str(i))
-        # lineSplit = i.split("\\")
+    for i in search(nameWay):
         lineSplit = re.findall(r'\w+|[-+]?(?:\d+(?:\.\d*)?|\.\d+)', str(i))
-        # print(lineSplit)
         try:
             saver = abs(int(float(lineSplit[6])))
         except Exception:
@@ -42,10 +45,8 @@ def parserFirst(nameFile, group, folder):
 
         if lineSplit[group] == nameFile:
             hubSlave.append("FILE_FOLDER:\t" + lineSplit[folder] + " PIC: " + str(saver) + "\t" + read_from_pathtxt(i) + "\n")
-
             counter += 1
         if counter == 60:
-            print("HERE")
             hubSlave = transformTextForNumberInCountValue(number1=1, number2=3, hub=hubSlave)
             hubSlave = sorted(hubSlave, key=itemgetter(3))
 
@@ -53,9 +54,6 @@ def parserFirst(nameFile, group, folder):
                 hubMain.append(hubSlave[i])
             hubSlave.clear()
             counter = 1
-    # for i in range(len(hubMain)):
-    #     print("Main\t", hubMain[i])
-
 
     # Сортировка строк по значению параметров
     hubMain = sorted(hubMain, key=itemgetter(1))
@@ -67,30 +65,40 @@ def parserFirst(nameFile, group, folder):
             if hubMain[i][1] != 'caption_base_best' and hubMain[i][1] != 'caption_huge_best':
                 hubMain[i][1] = str(hubMain[i][1])
 
-    # for i in range(60):
-    #     print(hubMain[i])
-
     with open('E:\Нейронки\Parser\\files\\' + nameFile + '.txt', 'w') as file:
         for i in range(len(hubMain)):
             file.write(' '.join(hubMain[i]) + '\n')
+    return 0
 
-def searcherParams():
+def searcherParams(nameWay, positionGroup):
     param, groupNew = [], 'beam'
-    for i in search():
+    for i in search(nameWay):
         lineSplit = re.findall(r'\w+', i)
-        if lineSplit[3] != groupNew:
+        if lineSplit[positionGroup] != groupNew:
             param.append(groupNew)
-            groupNew = lineSplit[3]
+            groupNew = lineSplit[positionGroup]
             if groupNew == 'unnormalized':
                 param.append(groupNew)
     return param
 
+def similarStrings(nameWay, nameLine):
+    cloud = []
+    for i in search(nameWay):
+        for fileReadLine in readFileForAnalysys(i):
+            try:
+                if (re.search(nameLine, fileReadLine)).group() == nameLine:
+                    cloud.append('true')
+            except Exception:
+                cloud.append('false')
+        print(cloud)
+        cloud.clear()
 def main():
-    params = searcherParams()
-    for j in range(len(params)):
-        parserFirst(nameFile=params[j], group=3, folder=4)
-
-
+    # params = searcherParams(nameWay="E:\Нейронки\\r4", positionGroup=3)
+    # for j in range(len(params)):
+    #     parserFirst(nameWay="E:\Нейронки\\r4", nameFile=params[j], group=3, folder=4)
+    way = "E:\Нейронки\Parser\\files"
+    # params = searcherParams(nameWay=way, positionGroup=4)
+    similarStrings(nameWay=way, nameLine='man')
 
 
 if __name__ == '__main__':
